c04b886b362cc0113b8ba8f08bc74e7af2241fd5
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-05-23 17:24:10 +0300

    aü

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const path = require('path')
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 600,
+        height: 400,
         webPreferences: {
+            resizable: false,
             nodeIntegration: true,
             contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
         }
     })
-
+    mainWindow.setResizable(false)
+    mainWindow.setMenuBarVisibility(false)
     // and load the index.html of the app.
     mainWindow.loadFile('index.html')
 
